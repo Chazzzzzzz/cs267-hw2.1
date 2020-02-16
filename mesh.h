57075432be28536b663822b2d48ea3faccd06477
Chaz <cuichangze@cuichangzes-MacBook-Pro.local>
2020-02-15 22:16:54 -0800

    delete inline

--- mesh.h
+++ mesh.h
@@ -5,17 +5,17 @@ using namespace std;
 class Mesh {
 private:
     void apply_force(particle_t& particle, particle_t& neighbor);
-    inline void apply_force_single_cell(particle_t* particle, int r, int c);
-    inline int get_cellRow(particle_t p);
-    inline int get_cellColumn(particle_t p);
+    void apply_force_single_cell(particle_t* particle, int r, int c);
+    int get_cellRow(particle_t p);
+    int get_cellColumn(particle_t p);
 public:
     int nRow, nColumn;
 
     double cellSize;
     vector<particle_t*>* cells;
     void initialize(int row, int column, double cell_size);
-    inline void generate_mesh(particle_t* parts, int num_parts);
-    inline void Insert(particle_t * p, int r, int c) {
+    void generate_mesh(particle_t* parts, int num_parts);
+    void Insert(particle_t * p, int r, int c) {
         cells[r * nRow + c].push_back(p);
     }
     inline void Clear() {
@@ -26,6 +26,6 @@ public:
         }
     }
     void apply_force_neighbor_cell(int r, int c);
-    inline void apply_force_mesh();
+    void apply_force_mesh();
 };
 
